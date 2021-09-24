bcff015650c9a7b08d8a0accdf4cafa2ad07076f
sadulla <ubaydullayevsadulla843@gmail.com>
2021-09-24 09:28:51 +0500

    My notes

---
+++ app/src/main/java/com/intentsoft/mynotes/fragments/HomeFragment.kt
@@ -0,0 +1,53 @@
+package com.intentsoft.mynotes.fragments
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.intentsoft.mynotes.R
+import com.intentsoft.mynotes.databinding.FragmentHomeBinding
+
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentHomeBinding.inflate(
+            inflater,container, false
+        )
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.fabAddNote.setOnClickListener { mView->
+            mView.findNavController().navigate(R.id.action_homeFragment_to_newNoteFragment)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.home_menu, menu)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+}
